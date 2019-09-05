b11280e390bafc31ed116395c752a622c8b9a480
yifei-tang <yifeit.tang@mail.utoronto.ca>
2019-09-05 18:06:17 -0400

    transitions added to projects slide

--- script.js
+++ script.js
@@ -11,12 +11,13 @@ var Project = class{
 
         
 
-        var myHTML='<div class="project-wrapper"> <h4>%title%</h4> <p>Technologies Used: %tech%</p> <p>%description%</p><ul id="%project-list%"></ul><div class="video-wrapper"> <iframe width="560" height="315" src="%videoURL%" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> </div> <a href="%github%" target="_blank", class="project-link" id="%git-id%"><i class="fab fa-github"></i> Click Here To See The Github Repository </a> </div>';
+        var myHTML='<div class="project-wrapper" id="%pro-id%"><span></span> <h4>%title%</h4> <p>Technologies Used: %tech%</p> <p>%description%</p><ul id="%project-list%"></ul><div class="video-wrapper"> <iframe width="560" height="315" src="%videoURL%" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> </div> <a href="%github%" target="_blank", class="project-link" id="%git-id%"><i class="fab fa-github"></i> Click Here To See The Github Repository </a> </div>';
         var newHTML=myHTML.replace('%title%',this.name);
         newHTML=newHTML.replace('%tech%',this.tech);
         newHTML=newHTML.replace('%description%',this.description);
         newHTML=newHTML.replace('%videoURL%',this.videoURL);
         newHTML=newHTML.replace('%github%',this.githubRepo);
+        newHTML=newHTML.replace('%pro-id%',this.projectID);
         newHTML=newHTML.replace('%git-id%','git'+this.projectID);
         newHTML=newHTML.replace('%project-list%','list'+this.projectID);
 
@@ -34,7 +35,26 @@ var Project = class{
         else{
             this.addList();
         }
+
+        //this.transition();
+
     }
+
+    // transition(){
+    //     tl.from("#"+this.projectID,1,{x:200, opacity:0});
+    //     tl.from('span',0.5,{width:0},"=-0.5");
+
+
+    //     const scene=new ScrollMagic.Scene({
+    //         triggerElement: "#"+this.projectID,
+    //         triggerHook: "onLeave",
+    //         offset: 30,
+    //         duration: 30,
+    //     })
+    //     //.setTween('#'+this.projectID, {x: '-50%'})
+    //     .setPin("#"+this.projectID).addTo(controller);
+
+    // }
 
     addList(){
         var ul = document.getElementById('list'+this.projectID);
@@ -52,6 +72,9 @@ var Project = class{
     }
 }
 
+var tl= new TimelineMax({onUpdate:updatePercentage});
+const controller= new ScrollMagic.Controller();
+
 var paragraphOne='PillSafe is an application built to classify medication using computer vision and machine learning techniques. I developed this as part of a research internship with the National University of Singapore. Users can enter a pill into the database with a single click of the camera. Users can also remove pills and re-insert them to account for the continually changing nature of medical advances.';
 var paragraphTwo="A semester long mapping application dedicated towards assisting tourists in finding their way around a new city. This GIS is designed to be visually appealing and find routes efficiently! We programmed this application in a team of 3 in C++, using GTK for the GUI. Features I programmed specifically include:";
 var paragraphThree="An arcade game developed in Verilog. Object of the game is to \"catch\" the stars and \"avoid\" the asteroids with the spaceship. I worked on this project with a partner for my Digital Systems course as a final project. My role involved creating an algorithm to randomly generate stars and asteroids, as well as writing a Finite State Machine to control game state. ";
@@ -60,3 +83,23 @@ var PillSafe = new Project('PillSafe', ['Python', ' OpenCV', ' Tensorflow', ' My
 var Onion = new Project('Onion GIS', ['C++', ' GTK', ' Dark Sky API', ' Open Street Maps API'], paragraphTwo,['Webscrapping live weather data using Dark Sky API and displaying it when the map is loaded.', "Using Dijkstra's algorithm to find the shortest path between locations in a city.", "Displaying popular locations such as restaurants, gas stations and schools with icons."],'',"https://www.youtube.com/embed/fzdEG83E4oo",'pro-2');
 var PillSafe = new Project('PillSafe', ['Verilog', ' PS-2 Controller', ' DE1-SoC Board'], paragraphThree,[], "https://github.com/yifei-tang/Star-Catcher-Project",'https://www.youtube.com/embed/c-0cN4G2e08','pro-3');
 
+tl.from("#pro-1",1,{x:200, opacity:0});
+tl.from("#pro-2",1,{x:200, opacity:0});
+tl.from("#pro-3",1,{x:200, opacity:0});
+
+tl.from('span',0.5,{width:0},"=-0.5");
+
+
+const scene=new ScrollMagic.Scene({
+    triggerElement: "#pro-1",
+    triggerHook: "onLeave",
+    offset: 30,
+    duration: 30,
+})
+.setTween('#pro-1', {x: '-50%'})
+.setPin("#pro-1").addTo(controller);
+
+
+function updatePercentage(){
+    tl.progress();
+}
